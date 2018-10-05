07d9a08ec6b3bc3412010007ac0f7ae78c1a2bb6
dancrump1 <42187710+dancrump1@users.noreply.github.com>
2018-10-05 14:36:22 -0400

    working well game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,15 @@
 //Array for computer to choose from a-z
 var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-//Computer chooses from array
+//Computer chooses from array and assigns to a variable
 var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+console.log(computerGuess);
 // Create variables that hold references to the places in the HTML where we want to display things.
 var userChoiceText = document.getElementById("userchoice-text");
 var computerChoiceText = document.getElementById("computerchoice-text");
 var winsText = document.getElementById("wins-text");
 var lossesText = document.getElementById("losses-text");
 var guessesText = document.getElementById("guesses-text");
-
 // Creating variables to hold the number of wins, losses, and guesses. They start at 0 while guesses starts at 9.
 var winsCounter = 0;
 var lossesCounter = 0;
@@ -21,10 +21,11 @@ lossesText.textContent = "losses: " + lossesCounter;
 winsText.textContent = "wins: " + winsCounter;
 guessesText.textContent = "guesses remaining: " + guessesCounter;
 
-
 alert("Guess a letter to start the game!")
 
 //Game logic
+
+//winning needs to increase wins by 1, display what the computer guessed, and reset the userGuesses array
 function winning() {
     winsCounter++;
     winsText.textContent = "wins: " + winsCounter;
@@ -37,11 +38,16 @@ function winning() {
     //Computer chooses from array
     computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
     console.log(computerGuess)
+    return 0;
 }
+
+//Guessing wrong needs to decrease guessesCounter by one and change the displayed number of guesses remaining
 function guessWrong() {
     guessesCounter--;
     guessesText.textContent = "Guesses remaining: " + guessesCounter;
 }
+
+//After guesses remaining = 0, a loss needs to be added to the counter and the computer needs to select a new letter. guesses counter and user input array also resets
 function losing() {
     alert("You didn't guess the letter I was thinking of! HAHA");
     lossesCounter++;
@@ -52,9 +58,7 @@ function losing() {
     guessesText.textContent = "Guesses remaining: " + guessesCounter;
     userGuesses.length = 0;
     userChoiceText.textContent = "You chose: " + userGuesses;
-
-
-
+    return 0;
 }
 
 // Runs whenever the user presses a key. 
@@ -69,9 +73,6 @@ document.onkeyup = function (event) {
     userChoiceText.textContent = "You chose: " + userGuesses;
     computerChoiceText.textContent = "The computer chose: ";
 
-
-
-
     // If userguess is computerGuess, wins increases by one
     if (userGuess == computerGuess) {
         winning();
@@ -83,4 +84,3 @@ document.onkeyup = function (event) {
         guessWrong();
     }
 }
-
